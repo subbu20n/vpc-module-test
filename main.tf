365d74f2d64b0a4c5389442d1bbf24cf9febcb7c
subbu <subbunandyala48@gmail.com>
2025-06-24 20:09:15 +0530

    terrafom

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/daws-84s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/subbu20n/terraform-aws-vpc.git?ref=main"
     /* project = "roboshop"
     environment = "dev"
     public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
